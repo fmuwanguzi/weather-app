7126fba66e09ee531aa0111cc6b8ed6d646ce2aa
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-30 14:53:50 -0500

    api access issues

--- routes/weather.js
+++ routes/weather.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const axios = require('axios');
+require('dotenv').config()
+
+let city = 'boston'
+let API_key = process.env.API_KEY;
+
+let url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`
 
 router.get('/weather', (req, res) => {
-    res.render('weather');
+    axios.get(url)
+    .then((response) => {
+        const myWeather = respose.data
+        
+        res.render('weather', {myWeather})
+
+    })
+    //res.render('weather');
 });
 
 module.exports = router;
