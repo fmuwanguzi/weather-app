d240e1eba406341af72a822ef27d742f616a727c
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-30 19:47:07 -0500

    started creating a form

--- routes/weather.js
+++ routes/weather.js
@@ -4,9 +4,9 @@ const db = require('../models');
 const axios = require('axios');
 require('dotenv').config()
 
-let city = 'boston'
+let city = 'new york'
 let API_KEY = process.env.API_KEY;
-//console.log(API_KEY);
+//console.log(API_KEY); to test thats its printing out correctly
 
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 
@@ -14,15 +14,15 @@ let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API
 router.get('/weather', (req, res) => {
     axios.get(url)
     .then((response) => {
-        //const myWeather = response
+        const myWeather = response.data
+        //testing to see data
         console.log(response.data)
-        //res.render('weather', {myWeather})
+        res.render('weather', {myWeather})
     })
     .catch(error => {
         console.log(error);
         res.send(error = "refresh your screen");
     });
-    //res.render('weather', {myWeather});
 });
 
 module.exports = router;
