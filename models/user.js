9d71d3b0311df32f48f29a1a6fd90d75ef3b042a
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-02 10:29:51 -0500

    [fixed a model problem that wasn't allowing user to login

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.user.hasMany(models.user)
+      models.user.hasMany(models.weather)
     }
   };
   user.init({
