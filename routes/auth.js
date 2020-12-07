38d7155e0ea761189236ee12dc1b6c9fd06e5135
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-07 09:43:33 -0500

    redirects to weather page

--- routes/auth.js
+++ routes/auth.js
@@ -47,7 +47,7 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
+  successRedirect: '/weather',
   failureRedirect: '/auth/login',
   successFlash: 'Welcome back...',
   failureFlash: 'Either email or password is incorrect. Please try again.'
